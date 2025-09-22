07e4443d5c46db7e15f3148b66fe76200aad449c
Paltaa69 <martin.pizarro@estudiantes.colegiotgs.cl>
2025-09-22 15:37:59 -0300

    Delete pokedex.py

--- pokedex.py
+++
@@ -1,5 +0,0 @@
-# importación de librerias
-import streamlit as st
-# configuración de página(título, ícono, logo,zona de navegación)
-st.set_page_config(page_title="Pokedex", page_icon=":no_entry:")
-st.logo("pokeball.png")
